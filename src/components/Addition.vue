02df03b74f849fef89952c101c8e66351c9db3d6
Now_Sprouting <15542456716@163.com>
2020-05-27 16:23:11 +0800

    vuex的demo1完成

--- src/components/Addition.vue
+++ src/components/Addition.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
-    <h2>当前最新的count值</h2>
-    <button>+!</button>
+    <!-- <h2>当前最新的count值{{$store.state.count}}</h2> -->
+    <h1>{{$store.getters.showNum}}</h1>
+    <button @click="handel1">+1</button>
+    <button @click="handel2">+N</button>
+    <button @click="handel3">延时+1</button>
+    <button @click="handel4">延时+N</button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    handel1() {
+      this.$store.commit('add')
+    },
+    handel2() {
+      this.$store.commit('addN', 3)
+    },
+    handel3() {
+      this.$store.dispatch('addAsync')
+    },
+    handel4() {
+      this.$store.dispatch('addNAsync', 3)
+    }
+  }
+}
 </script>
 
 
--- src/components/Subtraction.vue
+++ src/components/Subtraction.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
-    <h2>当前最新的count值</h2>
-    <button>-1</button>
+    <!-- <h2>当前最新的count值{{count}}</h2> -->
+    <h1>{{showNum}}</h1>
+    <button @click="handel1">-1</button>
+    <button @click="subN(3)">-N</button>
+    <button @click="subAsync">延时-1</button>
+    <button @click="subNAsync(3)">延时-N</button>
   </div>
 </template>
 
 <script>
-export default {};
+import { mapState, mapMutations, mapActions, mapGetters } from 'vuex'
+export default {
+  data() {
+    return {}
+  },
+  computed: {
+    ...mapState(['count']),
+    ...mapGetters(['showNum'])
+  },
+  methods: {
+    ...mapMutations(['sub', 'subN']),
+    ...mapActions(['subAsync', 'subNAsync']),
+    handel1() {
+      this.sub()
+    }
+  }
+}
 </script>
 
 
